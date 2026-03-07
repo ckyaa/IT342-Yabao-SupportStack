08fa525c2a526cddb3a63318118da815cbf9ce8a
ckyaa <ckyabao@gmail.com>
2026-03-07 11:59:15 +0800

    chore: initialize Spring Boot Maven backend

---
+++ backend/supportstack/src/test/java/edu/cit/yabao/supportstack/SupportstackApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.yabao.supportstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
